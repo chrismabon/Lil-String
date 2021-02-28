1dce5c1251ec017600369abd5de85ddac088af6f
Chris Mabon <chris.mabon@protonmail.com>
2021-02-27 19:52:41 -0500

    update source file license headers

--- LilStr.cpp
+++ LilStr.cpp
@@ -1,22 +1,17 @@
-/**
- * LilStr.cpp
- *
- * Copyright (C) 2020 Umbra Aeterna Labs
- * This file is part of Lil String <https://github.com/Umbra-Aeterna-Labs/Lil-String>.
- *
- * Lil String is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Lil String is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Lil String.  If not, see <http://www.gnu.org/licenses/>.
- */
+//  Copyright 2021 Chris Mabon <https://github.com/chrismabon>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <climits>
 #include <cmath>
